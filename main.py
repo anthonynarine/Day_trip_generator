a76c8ec3ba825da017b27f1a42f81860f22ac978
anthonynarine <anarine83@gmail.com>
2022-09-02 16:19:45 -0400

    Created lists of restaurants in each city and a function to randomly suggest a restaurant to the user

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 
 import random
 
-
 #list of famous cities.
-cities = ["Newy York, New York", "Osaka, Japan", "Amsterdam, Netherlands",  "Toronto, Canada", "Frankfurt, Germany", "Vienna, Austria", "Georgetwon, Guyana",]
+cities = ["New York", "Osaka", "Amsterdam", "Frankfurt", "Georgetwon",]
 
-#list of  mode of transportation.
+#list of Restaurant options in each city. 
+new_york_restaurants = ["A&A Bakery", "Home town BBQ", "Kochi",]
+osaka_restaurants = ["Awajishima Burger", "Byakuan", "Yonemasu",]
+amsterdam_restaurants = ["Jansz", "Wilde Zwijnen", "Stork",]
+frankfurt_restaurants = ["Emma Metzler", "Charirs", "No.16",]
+georgetown_restaurants = ["New Thriving", "Island Express", "Hibiscus",]
+
+
+#list of mode of transportation.
 type_of_transportation = ["Airplane", "Rail train", "Rental car", "Taxi", "Boat", "Walking", "Biking", ]
+
 
 
 def greet_user():
@@ -30,6 +38,7 @@ while True:
         print (f"\nLet's set you up with transportation.")
         break 
 
+
 def mode_of_transportation():
     """Randomly suggest a mode of transportation for the user"""
 while True:
@@ -40,13 +49,74 @@ while True:
         continue
     elif user_transport_choice == "Yes":
         print(f"\n{transportation} is a great way to explore {destination}")
-        print (f"\nLet's fined some fun things to do in {destination}.")
         break
-mode_of_transportation()
+
+def top_rated_restaurant_in_cities ():
+    """Randomly suggest a restaurant to the user"""
+    print (f"Let find a place a nice restaurant")
+while True:
+    if destination == "New York":
+        restaurant = random.choice(new_york_restaurants)
+        user_restaurant_choice = input (f"\nWould you like to try {restaurant}? Enter Yes/No ").capitalize()
+        if user_restaurant_choice == "No":
+            print (f"Ok, lets try a different place")
+            continue
+        else:
+             user_restaurant_choice == "Yes"
+             print (f"{restaurant} is a great place to eat in {destination}")
+             break
+    if destination == "Osaka":
+        restaurant = random.choice(osaka_restaurants)
+        user_restaurant_choice = input (f"\nWould you like to try {restaurant}? Enter Yes/No ").capitalize()
+        if user_restaurant_choice == "No":
+            print (f"Ok, lets try a different place")
+            continue
+        else:
+             user_restaurant_choice == "Yes"
+             print (f"{restaurant} is a great place to eat in {destination}")
+             break
+    if destination == "Amsterdam":
+        restaurant = random.choice(amsterdam_restaurants)
+        user_restaurant_choice = input (f"\nWould you like to {restaurant}? Enter Yes/No ").capitalize()
+        if user_restaurant_choice == "No":
+            print (f"Ok, lets try a different place")
+            continue
+        else:
+             user_restaurant_choice == "Yes"
+             print (f"{restaurant} is a great place to eat in {destination}")
+             break
+    if destination == "Frankfurt":
+        restaurant = random.choice(frankfurt_restaurants)
+        user_restaurant_choice = input (f"\nWould you like to try {restaurant}? Enter Yes/No ").capitalize()
+        if user_restaurant_choice == "No":
+            print (f"Ok, lets try a different place")
+            continue
+        else:
+             user_restaurant_choice == "Yes"
+             print (f"{restaurant} is a great place to eat in {destination}")
+             break
+    if destination == "Georgetwon":
+        restaurant = random.choice(georgetown_restaurants)
+        user_restaurant_choice = input (f"\nWould you like to try {restaurant}? Enter Yes/No ").capitalize()
+        if user_restaurant_choice == "No":
+            print (f"Ok, lets try a different place")
+            continue
+        else:
+             user_restaurant_choice == "Yes"
+             print (f"{restaurant} is a great place to eat in {destination}")
+             break
+
+    
+        
 
 
 
-
+# def run():
+#     greet_user()
+#     user_destination_choice
+#     mode_of_transportation
+#     top_rated_restaurant_in_cities
+# run()
 
 
 
